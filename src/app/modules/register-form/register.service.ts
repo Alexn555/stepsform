534fe527bd8655bc87be05cb9252cafc0d27ee74
Alex555\Alex <denver55all@hotmail.com>
2018-12-12 18:31:34 +0200

    Removed unsuded code, since same code in form service

--- src/app/modules/register-form/register.service.ts
+++ src/app/modules/register-form/register.service.ts
@@ -17,36 +17,4 @@ export class AppRegisterService {
     }));
   }
 
-  getCorrectFormatConfig(formItems, translationService) {
-    for (let i = 0; i < formItems.length; i++) {
-      formItems[i].label = this.getLabelTranslation(formItems[i].label, translationService);
-      formItems[i].validation = this.transformValidationFromArrayToFunctions(formItems[i].validation);
-    }
-    return formItems;
-  }
-
-  getLabelTranslation(label: string, translationService) {
-    return translationService.instant(label);
-  }
-
-  transformValidationFromArrayToFunctions(rawValidators) {
-    if (rawValidators.length <= 0) { return rawValidators; }
-
-    const rawValidatorArray = rawValidators[0];
-    for (const validator of Object.keys( rawValidatorArray )) {
-      if (validator === 'required') {
-        rawValidators.push(Validators.required);
-      }
-      if (validator === 'min') {
-        rawValidators.push(Validators.minLength(rawValidatorArray[validator]));
-      }
-      if (validator === 'max') {
-        rawValidators.push(Validators.maxLength(rawValidatorArray[validator]));
-      }
-    }
-
-    rawValidators.shift();
-    return rawValidators;
-  }
-
 }
